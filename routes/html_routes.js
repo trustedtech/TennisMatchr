7d01be21ffd9eb694f050a81f890545ecc356f09
SofaKB <hall.b17@gmail.com>
2020-03-28 11:16:35 -0400

    html routing

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,6 +1,14 @@
 module.exports = function(app) {
     app.get('/', function(req, res){
         res.render('index')
-    })
+    });
+
+    app.get('/event', function(req, res){
+        res.render('event')
+    });
+
+    app.get('/roster', function(req,res){
+        res.render('roster')
+    });
 }
 
