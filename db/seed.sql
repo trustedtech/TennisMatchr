cb5096c6c316ef4e46def74eb9019de08233bd96
SofaKB <hall.b17@gmail.com>
2020-03-31 17:26:46 -0400

    added production_config for ease of switching final db, capitalization fix

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,13 @@
-USE tennis_matchr_db;
+USE ylw935ilg6lase5h;
 
-INSERT INTO players (first_name, last_name, username, email, password, rating, createdAt, updatedAt)
+INSERT INTO Players (first_name, last_name, username, email, password, rating, createdAt, updatedAt)
 VALUES 
     ("Andre", "Agassi", "aagassi", "aagassi@tennis.com", "aa12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
     ("Pete", "Sampras", "psampras", "psampras@tennis.com", "ps12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
     ("Roger", "Federer", "rfederer", "rfederer@tennis.com", "rf12345", 7, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
     ("John", "McEnroe", "jmcenroe", "jmcenroe@tennis.com", "jm12345", 5, "2020-03-29T09:00:00", "2020-03-29T09:00:00");
 
-INSERT INTO events (creator, event_date, event_time, event_type, style, players_count, active, notes, createdAt, updatedAt)
+INSERT INTO Events (creator, event_date, event_time, event_type, style, players_count, active, notes, createdAt, updatedAt)
 VALUES 
     ("Andre Agassi","2020-03-30","3:00PM EST","match", "singles", 2, false, "Bring your own tennis balls.", "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
     ("Pete Sampras","2020-04-10","7:00PM EST","match", "doubles", 4, true, "Best of three sets.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
