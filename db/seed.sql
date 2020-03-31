656b6aa571f8c07ef1fcfd361d5dd0942ffcda8b
SofaKB <hall.b17@gmail.com>
2020-03-31 17:03:19 -0400

    removed id from seed (already implemented when adding to table) and lowered minimum password length

--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,17 @@
-USE tennis_matchr_db;
+USE tennismatchr_db;
 
-INSERT INTO players (id, first_name, last_name, username, email, password, rating, createdAt, updatedAt)
+INSERT INTO players (first_name, last_name, username, email, password, rating, createdAt, updatedAt)
 VALUES 
-    (1,"Andre", "Agassi", "aagassi", "aagassi@tennis.com", "aa12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (2,"Pete", "Sampras", "psampras", "psampras@tennis.com", "ps12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (3,"Roger", "Federer", "rfederer", "rfederer@tennis.com", "rf12345", 7, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (4,"John", "McEnroe", "jmcenroe", "jmcenroe@tennis.com", "jm12345", 5, "2020-03-29T09:00:00", "2020-03-29T09:00:00");
+    ("Andre", "Agassi", "aagassi", "aagassi@tennis.com", "aa12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Pete", "Sampras", "psampras", "psampras@tennis.com", "ps12345", 6, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Roger", "Federer", "rfederer", "rfederer@tennis.com", "rf12345", 7, "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("John", "McEnroe", "jmcenroe", "jmcenroe@tennis.com", "jm12345", 5, "2020-03-29T09:00:00", "2020-03-29T09:00:00");
 
-INSERT INTO events (id, creator, event_date, event_time, event_type, style, players_count, active, notes, createdAt, updatedAt)
+INSERT INTO events (creator, event_date, event_time, event_type, style, players_count, active, notes, createdAt, updatedAt)
 VALUES 
-    (1,"Andre Agassi","2020-03-30","3:00PM EST","match", "singles", 2, false, "Bring your own tennis balls.", "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (2,"Pete Sampras","2020-04-10","7:00PM EST","match", "doubles", 4, true, "Best of three sets.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (3,"Pete Sampras","2020-04-12","9:00AM EST","practice", "open", 6, true, "Reserved three courts.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (4,"Andre Agassi","2020-03-28","7:00PM EST","match", "singles", 2, false, null,"2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (5,"Roger Federer","2020-04-07","11:00AM EST","tournament", "singles", 10, true, "Email me to be added to the ladder.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
-    (6,"Roger Federer","2020-03-25","4:00PM EST","practice", "singles", 2, false, null,"2020-03-29T09:00:00", "2020-03-29T09:00:00");
+    ("Andre Agassi","2020-03-30","3:00PM EST","match", "singles", 2, false, "Bring your own tennis balls.", "2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Pete Sampras","2020-04-10","7:00PM EST","match", "doubles", 4, true, "Best of three sets.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Pete Sampras","2020-04-12","9:00AM EST","practice", "open", 6, true, "Reserved three courts.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Andre Agassi","2020-03-28","7:00PM EST","match", "singles", 2, false, null,"2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Roger Federer","2020-04-07","11:00AM EST","tournament", "singles", 10, true, "Email me to be added to the ladder.","2020-03-29T09:00:00", "2020-03-29T09:00:00"),
+    ("Roger Federer","2020-03-25","4:00PM EST","practice", "singles", 2, false, null,"2020-03-29T09:00:00", "2020-03-29T09:00:00");
